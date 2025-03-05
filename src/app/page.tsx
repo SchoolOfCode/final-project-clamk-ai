25c82cee6a44b4212a4a13965a2d7a1784b12875
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2025-03-05 19:05:55 +0000

    Feat positive-quote created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Carousel from "./components/Carousel";
+import PositiveQuotes from "./components/PositiveQuotes";
+
 
 export default function Home() {
   return (
     <div className="bg-custom-green text-custom-white">
       <Header />
+      <PositiveQuotes/>
       <Carousel />
       <Footer />
     </div>
