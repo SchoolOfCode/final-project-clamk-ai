65433c60934f91a333dc2c17df575cf9842d5147
Adam Hemmings <38017169+Makenshii@users.noreply.github.com>
2025-03-05 12:10:42 +0000

    initial back end set up

---
+++ src/app/api/data/route.js
@@ -0,0 +1,18 @@
+import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SECRET_KEY
+);
+
+export async function getCommunities() {
+  const { data, error } = await supabase.from("communities").select("*");
+
+  if (error) {
+    return Response.json({ error: error.message }, { status: 500 });
+  }
+  console.log(getCommunities);
+  return Response.json(data, { status: 200 });
+}
---
+++ src/middleware.js
@@ -0,0 +1,19 @@
+import { NextResponse } from "next/server";
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs";
+
+export async function middleware(req) {
+  const res = NextResponse.next();
+  const supabase = createMiddlewareClient({ req, res });
+
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  if (!session) return NextResponse.redirect(new URL("/login", req.url));
+
+  return res;
+}
+
+export const config = {
+  matcher: ["/protected-route/:path*"],
+};
