db4862ba0d59da1c1d3f27d64687bf1264044566
Luke Davies <luke.davies@junitech.com>
2025-03-05 14:12:52 +0000

    builds out dashboard components: carousel, header, footer

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] bg-custom-green items-center text-white justify-items-center p-8 pt-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col pt-10">
+        <Image
+          className="dark:invert"
+          src="/images/novari-logo.png"
+          alt="Novari Logo"
+          width={250}
+          height={300}
+        />
+
+        <h2 className="text-2xl"> Welcome to Our App</h2>
+      </main>
+    </div>
+  );
+}
