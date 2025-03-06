6b31fbd3828166612801b2a9fccc85adad7fc062
Luke Davies <luke.davies@junitech.com>
2025-03-06 10:31:37 +0000

    builds out profile page template

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import UserProfile from "../components/UserProfile";
 
 export default function Profile() {
   return (
     <div className="bg-custom-green text-white">
       <Header />
+      <UserProfile />
       <Footer />
     </div>
   );
