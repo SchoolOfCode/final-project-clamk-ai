93d5c279900e38d374a04686d8400539a82e0d7f
Maddy <184935886+Maddy-bee@users.noreply.github.com>
2025-03-06 16:56:06 +0000

    Styling communities page

--- src/app/components/CommunityCard.tsx
+++ src/app/components/CommunityCard.tsx
@@ -10,10 +10,15 @@ export default function CommunityCard({
   url,
 }: CommunityCardProps) {
   return (
-    <div className="bg-white text-custom-green rounded-md shadow p-4 m-2 hover:scale-103 hover:shadow-xl hover:rotate-1 transition-scale-shadow duration-100 ease-out">
-      <h3 className="font-medium text-center p-3">{name}</h3>
-      <p className="p-3">{purpose}</p>
-      <a className="text-gray-700" href={`${url}`}>
+    <div className="bg-white text-custom-green rounded-md shadow p-4 m-2 hover:scale-103 hover:shadow-xl hover:rotate-1 transition-scale-shadow duration-100 ease-out h-80 w-64 flex flex-col justify-center items-center">
+      <h3 className="font-bold text-black text-center p-3">{name}</h3>
+      <p className="text-black text-center p-3 flex-grow">{purpose}</p>
+      <a
+        className="font-bold text-blue-500 text-center"
+        href={`${url}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         See website
       </a>
     </div>
